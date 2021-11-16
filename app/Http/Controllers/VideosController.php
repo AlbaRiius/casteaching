31fbe882d0477081828fc10921d770eccd1b29d8
AlbaRiius <arius@iesebre.com>
2021-11-16 19:26:52 +0100

    Primer test verd

---
+++ app/Http/Controllers/VideosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideosController extends Controller
+{
+    public function show($id)
+    {
+////    dd($video->title);
+////    $video = new stdClass();
+////    $video->title = 'Ubuntu 101';
+////    $video->description = 'Here description';
+////    $video->published_at = 'December 13';
+///
+    return view('videos.show',[
+        'video' => Video::find($id)
+    ]); // CRUD -> RETRIEVE -> només un video
+    }
+}
--- tests/Feature/Videos/VideoTest.php
+++ tests/Feature/Videos/VideoTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature\Videos;
 
+use App\Models\Video;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class VideoTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * @test
      */
@@ -20,7 +23,6 @@ class VideoTest extends TestCase
             'description' => '# Here description',
             'url' => 'https://youtu.be/w8j07_DBl_I',
             'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
-            'completed' => false,
             'previous' => null,
             'next' => null,
             'series_id' => 1
