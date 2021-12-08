b860c68cedbc4a051ed93fdf299bd3073a1a7d8d
AlbaRiius <arius@iesebre.com>
2021-12-08 18:40:07 +0100

    114 CRUD 1 Implementem R de Retrieve o el mètode index

--- tests/Feature/Videos/VideosManageControllerTest.php
+++ tests/Feature/Videos/VideosManageControllerTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests\Feature\Videos;
 
+use App\Models\Video;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
+/**
+ * @covers \App\Http\Controllers\VideosManageController
+ */
 class VideosManageControllerTest extends TestCase
 {
     use RefreshDatabase;
@@ -14,14 +19,24 @@ class VideosManageControllerTest extends TestCase
      */
     public function user_with_permissions_can_manage_videos()
     {
-        //1
         $this->loginAsVideoManager();
 
-        // 2 executar
         $response = $this->get('/manage/videos');
 
-        // 3 Provar assert
+
+        $videos = create_sample_videos();
         $response->assertStatus(200);
+        $response->assertViewIs('videos.manage.index');
+        $response->assertViewHas('videos',function($v) use ($videos) {
+            return $v->count() === count($videos) && get_class($v) === Collection::class &&
+                get_class($videos[0]) === Video::class;
+        });
+
+        foreach ($videos as $video) {
+            $response->assertSee($video->id);
+            $response->assertSee($video->title);
+        }
+
     }
 
     /** @test */
