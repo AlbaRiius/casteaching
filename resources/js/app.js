e92aad0adbbce65ac6b8b374a9c5be4e58e3ee8a
AlbaRiius <arius@iesebre.com>
2022-01-18 16:51:40 +0100

    Video 101 creació paquet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import casteaching_arr from 'casteaching';
 
 window.Alpine = Alpine;
+window.casteaching = casteaching_arr;
 
 Alpine.start();
