2516cd404c0042eceb1a15b51321a15e45a0c13f
AlbaRiius <arius@iesebre.com>
2022-01-18 17:37:09 +0100

    102 Prova de la llibreria amb Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,19 @@
+import VideosList from "./components/VideosList";
+
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
 import casteaching_arr from 'casteaching';
+import Vue from 'vue';
 
 window.Alpine = Alpine;
 window.casteaching = casteaching_arr;
+window.Vue= Vue;
+
+window.Vue.component('videos-list', VideosList)
 
 Alpine.start();
+
+const app = new window.Vue({
+    el: '#app',
+});
