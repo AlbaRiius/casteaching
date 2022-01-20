4d6fc09e5df7f7eb51ee02dae798e981e7def3f2
AlbaRiius <arius@iesebre.com>
2022-01-20 15:29:33 +0100

    103 ImplementacióComponentsShowEditDestroy

--- casteaching_package/index.js
+++ casteaching_package/index.js
@@ -27,7 +27,7 @@ export default {
             return response.data
         },
         destroy: async function (id) {
-            const response = await apiClient.delete('/videos' + id)
+            const response = await apiClient.delete('/videos/' + id)
             return response.data
         },
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,17 @@
 import VideosList from "./components/VideosList";
+import Alpine from 'alpinejs';
+import casteaching from 'casteaching';
+import Vue from 'vue';
+import VideoForm from "./components/VideoForm";
 
 require('./bootstrap');
 
-import Alpine from 'alpinejs';
-import casteaching_arr from 'casteaching';
-import Vue from 'vue';
-
 window.Alpine = Alpine;
-window.casteaching = casteaching_arr;
-window.Vue= Vue;
+window.casteaching = casteaching;
+window.Vue = Vue;
 
-window.Vue.component('videos-list', VideosList)
-
+window.Vue.component('videos-list', VideosList )
+window.Vue.component('video-form', VideoForm )
 Alpine.start();
 
 const app = new window.Vue({
