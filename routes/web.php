e52a0c4452b96767a5cf227527af804f8c14fb3e
AlbaRiius <arius@iesebre.com>
2022-02-17 19:19:06 +0100

    132 Laravel Database Relationships Relació 1 a n series i videos

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ \App\Http\Controllers\LandingPageController::class,'show']);
+
 
 Route::get('/videos/{id}', [ VideosController::class,'show']);
 
