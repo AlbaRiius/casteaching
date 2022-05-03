fcbc109aa5b05b3a65a7c839e0f88bae34f7e688
AlbaRiius <arius@iesebre.com>
2022-05-03 19:12:05 +0200

    Arreglo alguns testos que em fallaven

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideosApiController;
+use App\Http\Controllers\SeriesApiController;
 
 /*
 |--------------------------------------------------------------------------
