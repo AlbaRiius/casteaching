5192db9662afbfbe42245011a3f74abb088e6e97
AlbaRiius <arius@iesebre.com>
2022-05-03 16:38:54 +0200

    canvis create_sample_series -> ja es veuen les imatges

--- app/Console/Commands/YoutubeImportPlaylist.php
+++ app/Console/Commands/YoutubeImportPlaylist.php
@@ -61,26 +61,16 @@ class YoutubeImportPlaylist extends Command
         $playlist = Youtube::getPlaylistById($this->argument('playlistId'));
         $this->info('Serie name: ' . $playlist->snippet->title);
         $this->info('Creating serie...');
-//            dd($playlist->snippet);
 
-//            $path = Storage::download($playlist->snippet->thumbnails->maxres->url);
-
-        $extension = pathinfo(storage_path(($playlist->snippet->thumbnails->maxres->url), PATHINFO_EXTENSION))['extension'];
-        $filename = Str::slug($playlist->snippet->title).'.'.$extension;
-        $path = 'app/public/series/' . $filename;
-        copy($playlist->snippet->thumbnails->maxres->url, storage_path($path));
-
-        //            dd(storage_path($path));
-//
-//        dd(Str::random(40));
-//            dd($path);
-//            dd($playlist->snippet->thumbnails->maxres->url);
-        //            https://i.ytimg.com/vi/ednlsVl-NHA/maxresdefault.jpg
+        $temp = tmpfile();
+        $path = stream_get_meta_data($temp)['uri']; // eg: /tmp/phpFx0513a
+        copy($playlist->snippet->thumbnails->maxres->url, $path);
+        $path = Storage::disk('public')->putFile('series', new File($path));
 
         $serie = Serie::create([
             'title' => $playlist->snippet->title,
             'description' => $playlist->snippet->description,
-            'image' => $filename,
+            'image' => $path,
             'teacher_name' => 'Sergi Tur Badenas',
             'teacher_photo_url' => 'https://www.gravatar.com/avatar/046889f49471fd40d105eb76b9d83bf6'
         ]);
@@ -101,7 +91,7 @@ class YoutubeImportPlaylist extends Command
                 'url' => 'https://www.youtube.com/embed/' . $item->snippet->resourceId->videoId,
                 'published_at' => Carbon::now(),
                 'previous' => optional($previous)->id,
-                'user_id' => User::where(['email' => 'arius@iesebre.com'])->first()->id,
+                'user_id' => User::where(['email' => 'fmunoz@iesebre.com'])->first()->id,
                 'serie_id' => $serie->id
             ]);
             if ($previous) {
